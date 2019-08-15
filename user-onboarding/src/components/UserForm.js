319c4d5d541be2fa4398a09fd75a2b306f4e2647
MicahJank <MicahJank@users.noreply.github.com>
2019-08-15 11:34:42 -0500

    imported the dependencies and changed Form.js to UserForm.js because Form is already used as a variable for formik

---
+++ user-onboarding/src/components/UserForm.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Field, withForkik } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const UserForm = () => {
+
+
+    return (
+
+        <div> This will be a form.</div>
+    );
+};
+
+export default UserForm;
