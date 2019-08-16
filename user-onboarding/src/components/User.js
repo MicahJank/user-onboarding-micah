cacf358d69a6dd932925cd68599ef9f6fc23d018
MicahJank <MicahJank@users.noreply.github.com>
2019-08-15 22:29:46 -0500

    styled the user card some more

--- user-onboarding/src/components/User.js
+++ user-onboarding/src/components/User.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPortrait } from '@fortawesome/free-solid-svg-icons';
+
 import styled from 'styled-components'; 
 
 
 const UserCard = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
     margin-bottom: 15px;
-    width: 250px;
-    padding: 20px;
     background-color: white;
     box-shadow: -4px 5px 38px -14px rgba(0,0,0,1);
+    border-radius: 10px;
+
+    .img-container {
+        padding: 30px 50px;
+        background-color: #F5F6F8;
+        border-radius: 50%;
+        margin: 50px;
+
+        .img {
+            font-size: 4rem;
+            color: #E7EAEF;
+        }
+    }
+
+    .user-info {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 250px;
+        width: 350px;
+        flex-direction: column;
+        background-color: #F5F6F8;
+        border-radius: 0 10px 10px 0;
+    }
 `;
 
 
@@ -16,8 +44,16 @@ const User = ( { userInfo, key } ) => {
 
     return (
         <UserCard>
-            <h2>{userInfo.name}</h2>
-            <h3>{userInfo.email}</h3>
+            <div className='img-container'>
+                <div className='img'>
+                    <FontAwesomeIcon icon={faPortrait} />
+                </div>
+            </div>
+            <div className='user-info'>
+                <h2>{userInfo.name}</h2>
+                <h3>{userInfo.email}</h3>
+            </div>
+           
         </UserCard>
     );
 };
--- user-onboarding/src/components/Users.js
+++ user-onboarding/src/components/Users.js
@@ -7,6 +7,8 @@ import User from './User.js';
 
 
 const UsersContainer = styled.div`
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
